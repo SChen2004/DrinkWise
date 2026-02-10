d507a22e0ddc5596e2d8e31a67a8e49d2cbd0d88
DavidLoaf <krljanovicdavid@gmail.com>
2026-02-10 11:57:35 -0800

    Created log Data and Save function

---
+++ app/src/main/java/com/example/cauds/data/model/LogData.kt
@@ -0,0 +1,16 @@
+package com.example.cauds.data.model
+
+import com.google.firebase.Timestamp
+
+
+data class LogData(
+    val userId: String = "",            
+    val timestamp: Timestamp? = null,             
+    val updatedAt: Long = 0L,
+
+    val drinkType: String = "",
+    val drinkAmount: Long = 0,
+    val drinkCount: Long = 0,
+    val drinkCost: Long = 0,
+    val alcoholByVolume: Long = 0,
+)
--- app/src/main/java/com/example/cauds/data/repository/StorageRepository.kt
+++ app/src/main/java/com/example/cauds/data/repository/StorageRepository.kt
@@ -1,7 +1,9 @@
 package com.example.cauds.data.repository
 
 import com.example.cauds.data.model.OnboardingData
+import com.example.cauds.data.model.LogData
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.Timestamp
 
 class StorageRepository {
     private val db = FirebaseFirestore.getInstance()
@@ -12,4 +14,17 @@ class StorageRepository {
             .addOnSuccessListener { onResult(true, null) }
             .addOnFailureListener { onResult(false, it.message) }
     }
+
+     fun saveLog(userId: String, logData: LogData, onResult: (Boolean, String?) -> Unit) {
+        val logWithMetadata = logData.copy(
+            userId = userId,
+            timestamp = logData.timestamp ?: Timestamp.now()
+        )
+
+        db.collection("logs")
+            .add(logWithMetadata) // auto-generates document ID
+            .addOnSuccessListener { onResult(true, null) }
+            .addOnFailureListener { onResult(false, it.message) }
+    }
+
 }
