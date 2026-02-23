d9c4f7543c967ddb4ab59b63df41a598e8f22d71
DavidLoaf <krljanovicdavid@gmail.com>
2026-02-22 19:33:49 -0800

    Added Calendar screen and view model, but they are not wired together yet.

--- app/src/main/java/com/example/cauds/data/repository/LogRepository.kt
+++ app/src/main/java/com/example/cauds/data/repository/LogRepository.kt
@@ -4,7 +4,6 @@ import com.example.cauds.data.model.LogData
 import com.example.cauds.data.model.LogItem
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.toObject
 
 const val LOGS = "logs"
 const val USER_ID = "userId"
